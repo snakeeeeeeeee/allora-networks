03356b3df661aab106c0ed15e5af592b700deab6
Tobi Okedeji <tobiokedeji@gmail.com>
2024-03-06 20:12:48 +0100

    make the script a loop instead

--- testnet/generate_head_workers.sh
+++ testnet/generate_head_workers.sh
@@ -42,93 +42,25 @@ else
         --secret-string "$headAlloraAccExport"
 fi
 
+# Array of worker names
+workers=("coin-prediction" "coin-prediction-randomized" "index-provider" "nft-appraisals" "watch-prices")
 
-echo "generating allora account key for coin-prediction"
-mkdir -p ./coin-prediction
-$ALLORAD --home=./coin-prediction keys add coin-prediction --keyring-backend $keyringBackend > ./coin-prediction/coin-prediction.account_info 2>&1
-cpAlloraAccExport=$($ALLORAD --home=./coin-prediction keys export coin-prediction --unarmored-hex --unsafe --keyring-backend $keyringBackend)
+for worker in "${workers[@]}"; do
+    echo "generating allora account key for $worker"
+    mkdir -p ./$worker
+    $ALLORAD --home=./$worker keys add $worker --keyring-backend $keyringBackend > ./$worker/$worker.account_info 2>&1
+    workerAlloraAccExport=$($ALLORAD --home=./$worker keys export $worker --unarmored-hex --unsafe --keyring-backend $keyringBackend)
 
-cpAlloraKeySecret="${CHAIN_ID}--coin-prediction--allora-account"
-echo "saving coin prediction allora key to $cpAlloraKeySecret"
-if aws secretsmanager describe-secret --secret-id $cpAlloraKeySecret --region $AWS_REGION > /dev/null 2>&1 ; then
-    aws secretsmanager put-secret-value \
-        --secret-id $cpAlloraKeySecret --region $AWS_REGION \
-        --secret-string "$cpAlloraAccExport"
-else
-    aws secretsmanager create-secret \
-        --name $cpAlloraKeySecret --region $AWS_REGION \
-        --description "coin prediction allorad account's key export" \
-        --secret-string "$cpAlloraAccExport"
-fi
-
-echo "generating allora account key for coin-prediction-randomized"
-mkdir -p ./coin-prediction-randomized
-$ALLORAD --home=./coin-prediction-randomized keys add coin-prediction-randomized --keyring-backend $keyringBackend > ./coin-prediction-randomized/coin-prediction-randomized.account_info 2>&1
-cprAlloraAccExport=$($ALLORAD --home=./coin-prediction-randomized keys export coin-prediction-randomized --unarmored-hex --unsafe --keyring-backend $keyringBackend)
-
-cprAlloraKeySecret="${CHAIN_ID}--coin-prediction-randomized--allora-account"
-echo "saving coin prediction randomized allora key to $cprAlloraKeySecret"
-if aws secretsmanager describe-secret --secret-id $cprAlloraKeySecret --region $AWS_REGION > /dev/null 2>&1 ; then
-    aws secretsmanager put-secret-value \
-        --secret-id $cprAlloraKeySecret --region $AWS_REGION \
-        --secret-string "$cprAlloraAccExport"
-else
-    aws secretsmanager create-secret \
-        --name $cprAlloraKeySecret --region $AWS_REGION \
-        --description "coin prediction randomized allorad account's key export" \
-        --secret-string "$cprAlloraAccExport"
-fi
-
-echo "generating allora account key for index-provider"
-mkdir -p ./index-provider
-$ALLORAD --home=./index-provider keys add index-provider --keyring-backend $keyringBackend > ./index-provider/index-provider.account_info 2>&1
-indexProviderAlloraAccExport=$($ALLORAD --home=./index-provider keys export index-provider --unarmored-hex --unsafe --keyring-backend $keyringBackend)
-
-indexProvAlloraKeySecret="${CHAIN_ID}--index-provider--allora-account"
-echo "saving index-provider allora key to $indexProvAlloraKeySecret"
-if aws secretsmanager describe-secret --secret-id $indexProvAlloraKeySecret --region $AWS_REGION > /dev/null 2>&1 ; then
-    aws secretsmanager put-secret-value \
-        --secret-id $indexProvAlloraKeySecret --region $AWS_REGION \
-        --secret-string "$indexProviderAlloraAccExport"
-else
-    aws secretsmanager create-secret \
-        --name $indexProvAlloraKeySecret --region $AWS_REGION \
-        --description "index-provider allorad account's key export" \
-        --secret-string "$indexProviderAlloraAccExport"
-fi
-
-echo "generating allora account key for nft-appraisals"
-mkdir -p ./nft-appraisals
-$ALLORAD --home=./nft-appraisals keys add nft-appraisals --keyring-backend $keyringBackend > ./nft-appraisals/nft-appraisals.account_info 2>&1
-nftAppraisalsAlloraAccExport=$($ALLORAD --home=./nft-appraisals keys export nft-appraisals --unarmored-hex --unsafe --keyring-backend $keyringBackend)
-
-nftAppraisalsAlloraKeySecret="${CHAIN_ID}--nft-appraisals--allora-account"
-echo "saving nft-appraisals allora key to $nftAppraisalsAlloraKeySecret"
-if aws secretsmanager describe-secret --secret-id $nftAppraisalsAlloraKeySecret --region $AWS_REGION > /dev/null 2>&1 ; then
-    aws secretsmanager put-secret-value \
-        --secret-id $nftAppraisalsAlloraKeySecret --region $AWS_REGION \
-        --secret-string "$nftAppraisalsAlloraAccExport"
-else
-    aws secretsmanager create-secret \
-        --name $nftAppraisalsAlloraKeySecret --region $AWS_REGION \
-        --description "nft-appraisals allorad account's key export" \
-        --secret-string "$nftAppraisalsAlloraAccExport"
-fi
-
-echo "generating allora account key for watch-prices"
-mkdir -p ./watch-prices
-$ALLORAD --home=./watch-prices keys add watch-prices --keyring-backend $keyringBackend > ./watch-prices/watch-prices.account_info 2>&1
-watchPricesAlloraAccExport=$($ALLORAD --home=./watch-prices keys export watch-prices --unarmored-hex --unsafe --keyring-backend $keyringBackend)
-
-watchPricesAlloraKeySecret="${CHAIN_ID}--watch-prices--allora-account"
-echo "saving watch-prices allora key to $watchPricesAlloraKeySecret"
-if aws secretsmanager describe-secret --secret-id $watchPricesAlloraKeySecret --region $AWS_REGION > /dev/null 2>&1 ; then
-    aws secretsmanager put-secret-value \
-        --secret-id $watchPricesAlloraKeySecret --region $AWS_REGION \
-        --secret-string "$watchPricesAlloraAccExport"
-else
-    aws secretsmanager create-secret \
-        --name $watchPricesAlloraKeySecret --region $AWS_REGION \
-        --description "watch-prices allorad account's key export" \
-        --secret-string "$watchPricesAlloraAccExport"
-fi
+    workerAlloraKeySecret="${CHAIN_ID}--$worker--allora-account"
+    echo "saving $worker allora key to $workerAlloraKeySecret"
+    if aws secretsmanager describe-secret --secret-id $workerAlloraKeySecret --region $AWS_REGION > /dev/null 2>&1 ; then
+        aws secretsmanager put-secret-value \
+            --secret-id $workerAlloraKeySecret --region $AWS_REGION \
+            --secret-string "$workerAlloraAccExport"
+    else
+        aws secretsmanager create-secret \
+            --name $workerAlloraKeySecret --region $AWS_REGION \
+            --description "$worker allorad account's key export" \
+            --secret-string "$workerAlloraAccExport"
+    fi
+done
