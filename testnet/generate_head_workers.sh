4a5fd6fdedf4de11950c73e6ce7dacb7d308f1d1
Tobi Okedeji <tobiokedeji@gmail.com>
2024-03-06 19:13:36 +0100

    added coin prediction randomized

--- testnet/generate_head_workers.sh
+++ testnet/generate_head_workers.sh
@@ -8,55 +8,73 @@ AWS_REGION="us-east-1"
 ALLORAD="/usr/local/bin/allorad"
 keyringBackend=test
 
-echo "initializing head configs"
-docker run -it --entrypoint=bash -v "./head":/data 696230526504.dkr.ecr.us-east-1.amazonaws.com/allora-inference-base:dev-latest -c "mkdir -p /data/key && cd /data/key && allora-keys"
-cat ./head/key/priv.bin | base64 > ./head/key/priv.base64
+# echo "initializing head configs"
+# docker run -it --entrypoint=bash -v "./head":/data 696230526504.dkr.ecr.us-east-1.amazonaws.com/allora-inference-base:dev-latest -c "mkdir -p /data/key && cd /data/key && allora-keys"
+# cat ./head/key/priv.bin | base64 > ./head/key/priv.base64
 
-headKeySecret="${CLUSTER_DEPLOYED}--${CHAIN_ID}-heads--head-0--p2p-privatekey"
-echo "saving head p2p privatekey to $headKeySecret"
-if aws secretsmanager describe-secret --secret-id $headKeySecret --region $AWS_REGION > /dev/null 2>&1 ; then
+# headKeySecret="${CLUSTER_DEPLOYED}--${CHAIN_ID}-heads--head-0--p2p-privatekey"
+# echo "saving head p2p privatekey to $headKeySecret"
+# if aws secretsmanager describe-secret --secret-id $headKeySecret --region $AWS_REGION > /dev/null 2>&1 ; then
+#     aws secretsmanager put-secret-value \
+#         --secret-id $headKeySecret --region $AWS_REGION \
+#         --secret-string file://head/key/priv.base64
+# else
+#     aws secretsmanager create-secret \
+#         --name $headKeySecret --region $AWS_REGION \
+#         --description "head priv.bin in base 64" \
+#         --secret-string file://head/key/priv.base64
+# fi
+
+# echo "generating allora account key for head"
+# $ALLORAD --home=./head keys add head --keyring-backend $keyringBackend > ./head/head.account_info 2>&1
+# headAlloraAccExport=$($ALLORAD --home=./head keys export head --unarmored-hex --unsafe --keyring-backend $keyringBackend)
+
+# headAlloraKeySecret="${CLUSTER_DEPLOYED}--${CHAIN_ID}-heads--head-0--allora-key"
+# echo "saving head allora key to $headAlloraKeySecret"
+# if aws secretsmanager describe-secret --secret-id $headAlloraKeySecret --region $AWS_REGION > /dev/null 2>&1 ; then
+#     aws secretsmanager put-secret-value \
+#         --secret-id $headAlloraKeySecret --region $AWS_REGION \
+#         --secret-string "$headAlloraAccExport"
+# else
+#     aws secretsmanager create-secret \
+#         --name $headAlloraKeySecret --region $AWS_REGION \
+#         --description "head allorad account's key export" \
+#         --secret-string "$headAlloraAccExport"
+# fi
+
+
+# echo "generating allora account key for coin-prediction"
+# mkdir -p ./coin-prediction
+# $ALLORAD --home=./coin-prediction keys add coin-prediction --keyring-backend $keyringBackend > ./coin-prediction/coin-prediction.account_info 2>&1
+# cpAlloraAccExport=$($ALLORAD --home=./coin-prediction keys export coin-prediction --unarmored-hex --unsafe --keyring-backend $keyringBackend)
+
+# cpAlloraKeySecret="${CHAIN_ID}--coin-prediction--allora-account"
+# echo "saving coin prediction allora key to $cpAlloraKeySecret"
+# if aws secretsmanager describe-secret --secret-id $cpAlloraKeySecret --region $AWS_REGION > /dev/null 2>&1 ; then
+#     aws secretsmanager put-secret-value \
+#         --secret-id $cpAlloraKeySecret --region $AWS_REGION \
+#         --secret-string "$cpAlloraAccExport"
+# else
+#     aws secretsmanager create-secret \
+#         --name $cpAlloraKeySecret --region $AWS_REGION \
+#         --description "coin prediction allorad account's key export" \
+#         --secret-string "$cpAlloraAccExport"
+# fi
+
+echo "generating allora account key for coin-prediction-randomized"
+mkdir -p ./coin-prediction-randomized
+$ALLORAD --home=./coin-prediction-randomized keys add coin-prediction-randomized --keyring-backend $keyringBackend > ./coin-prediction-randomized/coin-prediction-randomized.account_info 2>&1
+cprAlloraAccExport=$($ALLORAD --home=./coin-prediction-randomized keys export coin-prediction-randomized --unarmored-hex --unsafe --keyring-backend $keyringBackend)
+
+cprAlloraKeySecret="${CHAIN_ID}--coin-prediction-randomized--allora-account"
+echo "saving coin prediction randomized allora key to $cprAlloraKeySecret"
+if aws secretsmanager describe-secret --secret-id $cprAlloraKeySecret --region $AWS_REGION > /dev/null 2>&1 ; then
     aws secretsmanager put-secret-value \
-        --secret-id $headKeySecret --region $AWS_REGION \
-        --secret-string file://head/key/priv.base64
+        --secret-id $cprAlloraKeySecret --region $AWS_REGION \
+        --secret-string "$cprAlloraAccExport"
 else
     aws secretsmanager create-secret \
-        --name $headKeySecret --region $AWS_REGION \
-        --description "head priv.bin in base 64" \
-        --secret-string file://head/key/priv.base64
-fi
-
-echo "generating allora account key for head"
-$ALLORAD --home=./head keys add head --keyring-backend $keyringBackend > ./head/head.account_info 2>&1
-headAlloraAccExport=$($ALLORAD --home=./head keys export head --unarmored-hex --unsafe --keyring-backend $keyringBackend)
-
-headAlloraKeySecret="${CLUSTER_DEPLOYED}--${CHAIN_ID}-heads--head-0--allora-key"
-echo "saving head allora key to $headAlloraKeySecret"
-if aws secretsmanager describe-secret --secret-id $headAlloraKeySecret --region $AWS_REGION > /dev/null 2>&1 ; then
-    aws secretsmanager put-secret-value \
-        --secret-id $headAlloraKeySecret --region $AWS_REGION \
-        --secret-string "$headAlloraAccExport"
-else
-    aws secretsmanager create-secret \
-        --name $headAlloraKeySecret --region $AWS_REGION \
-        --description "head allorad account's key export" \
-        --secret-string "$headAlloraAccExport"
-fi
-
-
-echo "generating allora account key for coin-prediction"
-mkdir -p ./coin-prediction
-$ALLORAD --home=./coin-prediction keys add coin-prediction --keyring-backend $keyringBackend > ./coin-prediction/coin-prediction.account_info 2>&1
-cpAlloraAccExport=$($ALLORAD --home=./coin-prediction keys export coin-prediction --unarmored-hex --unsafe --keyring-backend $keyringBackend)
-
-cpAlloraKeySecret="${CHAIN_ID}--coin-prediction--allora-account"
-echo "saving coin prediction allora key to $cpAlloraKeySecret"
-if aws secretsmanager describe-secret --secret-id $cpAlloraKeySecret --region $AWS_REGION > /dev/null 2>&1 ; then
-    aws secretsmanager put-secret-value \
-        --secret-id $cpAlloraKeySecret --region $AWS_REGION \
-        --secret-string "$cpAlloraAccExport"
-else
-    aws secretsmanager create-secret \
-        --name $cpAlloraKeySecret --region $AWS_REGION \
-        --description "head allorad account's key export" \
-        --secret-string "$cpAlloraAccExport"
+        --name $cprAlloraKeySecret --region $AWS_REGION \
+        --description "coin prediction randomized allorad account's key export" \
+        --secret-string "$cprAlloraAccExport"
 fi
