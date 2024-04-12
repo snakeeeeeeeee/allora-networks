d864e81ba7d0dec1fb73dff65b1d1b2ff2e859f5
Tobi Okedeji <tobiokedeji@gmail.com>
2024-04-12 16:10:04 +0100

    new devnet genesis

--- devnet/generate_genesis.sh
+++ devnet/generate_genesis.sh
@@ -3,44 +3,56 @@ set -eu
 
 CHAIN_ID="devnet"
 DENOM="uallo"
-VALIDATOR_TOKENS=1000000
-FAUCET_TOKENS=1000000000000000000
-VALIDATOR_NUMBER=3    #! Used in save_keys_awssecretsmanager.sh
 
-ALLORAD=$(which allorad)
+UPSHOT_WALLET_NAME="upshot"
+UPSHOT_WALLET_TOKENS=$(echo '99*10^18' | bc)
+FAUCET_WALLET_NAME="faucet"
+FAUCET_WALLET_TOKENS=$(echo '10^18' | bc)
+
+VALIDATOR_TOKENS=$(echo '(10^26 - 100*10^18)/3' | bc)
+VALIDATOR_NUMBER=3                    
+
+allorad=$(which allorad)
 keyringBackend=test
 
-faucetAccount="faucet"
-
-valPreffix="val"       #! Used in save_keys_awssecretsmanager.sh  
+valPreffix="val" 
 alloraHome="./"
 gentxDir=${alloraHome}/gentxs
 mkdir -p $gentxDir
 
 # echo "$alloraHome"
-$ALLORAD --home=$alloraHome init mymoniker --chain-id $CHAIN_ID --default-denom ${DENOM}
+$allorad --home=$alloraHome init mymoniker --chain-id $CHAIN_ID --default-denom ${DENOM}
 
 #Create validators account
 for ((i=0; i<$VALIDATOR_NUMBER; i++)); do
     valName="${valPreffix}${i}"
 
     echo "Generate $valName account"
-    $ALLORAD --home=$alloraHome keys add $valName \
+    $allorad --home=$alloraHome keys add $valName \
         --keyring-backend $keyringBackend > $valName.account_info 2>&1
 
     echo "Fund $valName account to genesis"
-    $ALLORAD --home=$alloraHome genesis add-genesis-account \
+    $allorad --home=$alloraHome genesis add-genesis-account \
         $valName ${VALIDATOR_TOKENS}${DENOM} \
         --keyring-backend $keyringBackend
 done
 
-echo "Generate $faucetAccount account"
-$ALLORAD --home=$alloraHome keys add $faucetAccount \
-    --keyring-backend $keyringBackend > $faucetAccount.account_info 2>&1
+echo "Generate $UPSHOT_WALLET_NAME account"
+$allorad --home=$alloraHome keys add $UPSHOT_WALLET_NAME \
+    --keyring-backend $keyringBackend > $UPSHOT_WALLET_NAME.account_info 2>&1
 
-echo "Fund $faucetAccount account"
-$ALLORAD --home=$alloraHome genesis add-genesis-account \
-    $faucetAccount ${FAUCET_TOKENS}${DENOM} \
+echo "Fund $UPSHOT_WALLET_NAME account"
+$allorad --home=$alloraHome genesis add-genesis-account \
+    $UPSHOT_WALLET_NAME ${UPSHOT_WALLET_TOKENS}${DENOM} \
+    --keyring-backend $keyringBackend
+
+echo "Generate $FAUCET_WALLET_NAME account"
+$allorad --home=$alloraHome keys add $FAUCET_WALLET_NAME \
+    --keyring-backend $keyringBackend > $FAUCET_WALLET_NAME.account_info 2>&1
+
+echo "Fund $FAUCET_WALLET_NAME account"
+$allorad --home=$alloraHome genesis add-genesis-account \
+    $FAUCET_WALLET_NAME ${FAUCET_WALLET_TOKENS}${DENOM} \
     --keyring-backend $keyringBackend
 
 for ((i=0; i<$VALIDATOR_NUMBER; i++)); do
@@ -50,7 +62,7 @@ for ((i=0; i<$VALIDATOR_NUMBER; i++)); do
     valHome="./$valName"
     mkdir -p $valHome
 
-    $ALLORAD --home=$valHome init $valName --chain-id $CHAIN_ID --default-denom ${DENOM}
+    $allorad --home=$valHome init $valName --chain-id $CHAIN_ID --default-denom ${DENOM}
 
     # Symlink genesis to have the accounts
     gln -sfr config/genesis.json $valHome/config/genesis.json
@@ -58,14 +70,14 @@ for ((i=0; i<$VALIDATOR_NUMBER; i++)); do
     # Symlink keyring-test to have keys
     gln -sfr keyring-test $valHome/keyring-test
 
-    $ALLORAD --home=$valHome genesis gentx $valName ${VALIDATOR_TOKENS}${DENOM} \
+    $allorad --home=$valHome genesis gentx $valName ${VALIDATOR_TOKENS}${DENOM} \
         --chain-id $CHAIN_ID --keyring-backend $keyringBackend \
         --moniker="$valName" \
         --from=$valName \
         --output-document $gentxDir/$valName.json
 done
 
-$ALLORAD --home=$alloraHome genesis collect-gentxs --gentx-dir $gentxDir
+$allorad --home=$alloraHome genesis collect-gentxs --gentx-dir $gentxDir
 
 cp $alloraHome/config/genesis.json $alloraHome
 
