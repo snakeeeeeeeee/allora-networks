f95a4a19bfe957a366a8f97afc7d134abec2672f
vladupshot <154623109+vladupshot@users.noreply.github.com>
2024-04-20 07:55:55 +1200

    updatem testnet generate genesis script

--- testnet/generate_genesis.sh
+++ testnet/generate_genesis.sh
@@ -10,49 +10,48 @@ FAUCET_WALLET_NAME="faucet"
 FAUCET_WALLET_TOKENS=$(echo '10^18' | bc)
 
 VALIDATOR_TOKENS=$(echo '(10^26 - 100*10^18)/3' | bc)
-VALIDATOR_NUMBER=3                    #! Used in save_keys_awssecretsmanager.sh
+VALIDATOR_NUMBER=3                         #! Used in save_keys_awssecretsmanager.sh
+COMMON_HOME_DIR="${COMMON_HOME_DIR:-$(pwd)}"
 
-# allorad="/usr/local/bin/allorad"
 allorad=$(which allorad)
 keyringBackend=test
 
-valPreffix="val"                      #! Used in save_keys_awssecretsmanager.sh
-alloraHome="./"
-gentxDir=${alloraHome}/gentxs
+valPreffix="validator"                      #! Used in save_keys_awssecretsmanager.sh
+genesisHome="$COMMON_HOME_DIR/genesis"
+gentxDir=${genesisHome}/gentxs
 mkdir -p $gentxDir
 
-# echo "$alloraHome"
-$allorad --home=$alloraHome init mymoniker --chain-id $CHAIN_ID --default-denom ${DENOM}
+$allorad --home=$genesisHome init mymoniker --chain-id $CHAIN_ID --default-denom ${DENOM}
 
 #Create validators account
 for ((i=0; i<$VALIDATOR_NUMBER; i++)); do
     valName="${valPreffix}${i}"
 
     echo "Generate $valName account"
-    $allorad --home=$alloraHome keys add $valName \
-        --keyring-backend $keyringBackend > $valName.account_info 2>&1
+    $allorad --home=$genesisHome keys add $valName \
+        --keyring-backend $keyringBackend > $COMMON_HOME_DIR/$valName.account_info 2>&1
 
     echo "Fund $valName account to genesis"
-    $allorad --home=$alloraHome genesis add-genesis-account \
+    $allorad --home=$genesisHome genesis add-genesis-account \
         $valName ${VALIDATOR_TOKENS}${DENOM} \
         --keyring-backend $keyringBackend
 done
 
 echo "Generate $UPSHOT_WALLET_NAME account"
-$allorad --home=$alloraHome keys add $UPSHOT_WALLET_NAME \
-    --keyring-backend $keyringBackend > $UPSHOT_WALLET_NAME.account_info 2>&1
+$allorad --home=$genesisHome keys add $UPSHOT_WALLET_NAME \
+    --keyring-backend $keyringBackend > $COMMON_HOME_DIR/$UPSHOT_WALLET_NAME.account_info 2>&1
 
 echo "Fund $UPSHOT_WALLET_NAME account"
-$allorad --home=$alloraHome genesis add-genesis-account \
+$allorad --home=$genesisHome genesis add-genesis-account \
     $UPSHOT_WALLET_NAME ${UPSHOT_WALLET_TOKENS}${DENOM} \
     --keyring-backend $keyringBackend
 
 echo "Generate $FAUCET_WALLET_NAME account"
-$allorad --home=$alloraHome keys add $FAUCET_WALLET_NAME \
-    --keyring-backend $keyringBackend > $FAUCET_WALLET_NAME.account_info 2>&1
+$allorad --home=$genesisHome keys add $FAUCET_WALLET_NAME \
+    --keyring-backend $keyringBackend > $COMMON_HOME_DIR/$FAUCET_WALLET_NAME.account_info 2>&1
 
 echo "Fund $FAUCET_WALLET_NAME account"
-$allorad --home=$alloraHome genesis add-genesis-account \
+$allorad --home=$genesisHome genesis add-genesis-account \
     $FAUCET_WALLET_NAME ${FAUCET_WALLET_TOKENS}${DENOM} \
     --keyring-backend $keyringBackend
 
@@ -60,16 +59,16 @@ for ((i=0; i<$VALIDATOR_NUMBER; i++)); do
     echo "Initializing Validator $i"
 
     valName="${valPreffix}${i}"
-    valHome="./$valName"
+    valHome="$COMMON_HOME_DIR/$valName"
     mkdir -p $valHome
 
     $allorad --home=$valHome init $valName --chain-id $CHAIN_ID --default-denom ${DENOM}
 
     # Symlink genesis to have the accounts
-    ln -sfr config/genesis.json $valHome/config/genesis.json
+    ln -sfr $genesisHome/config/genesis.json $valHome/config/genesis.json
 
     # Symlink keyring-test to have keys
-    ln -sfr keyring-test $valHome/keyring-test
+    ln -sfr $genesisHome/keyring-test $valHome/keyring-test
 
     $allorad --home=$valHome genesis gentx $valName ${VALIDATOR_TOKENS}${DENOM} \
         --chain-id $CHAIN_ID --keyring-backend $keyringBackend \
@@ -78,8 +77,8 @@ for ((i=0; i<$VALIDATOR_NUMBER; i++)); do
         --output-document $gentxDir/$valName.json
 done
 
-$allorad --home=$alloraHome genesis collect-gentxs --gentx-dir $gentxDir
+$allorad --home=$genesisHome genesis collect-gentxs --gentx-dir $gentxDir
 
-cp $alloraHome/config/genesis.json $alloraHome
+cp $genesisHome/config/genesis.json $COMMON_HOME_DIR
 
 echo "$CHAIN_ID genesis generated."
